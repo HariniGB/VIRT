eccf6c6cb9bc12fdc4acf5f084e41f7bba63039d
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-11-21 12:17:50 -0800

    Update older version without openstack flavors list

--- server.go
+++ server.go
@@ -10,8 +10,6 @@ import (
   "gopkg.in/mgo.v2"
 )
 
-
-
 func main() {
   // Instantiate a new router
   r := httprouter.New()
@@ -32,7 +30,6 @@ func main() {
 func getSession() *mgo.Session {
   // Connect to our local mongo
   s, err := mgo.Dial("mongodb://localhost:27017")
-
   // Check if connection error, is mongo running?
   if err != nil {
     panic(err)
