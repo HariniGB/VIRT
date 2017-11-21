da8cee320a27623bf2437e72dc909ab319f22e86
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-11-21 10:37:16 -0800

    Add list of flavors in the controller and the Dashboard page

--- controllers/main.go
+++ controllers/main.go
@@ -5,10 +5,15 @@ import (
   "html/template"
   "fmt"
   "github.com/julienschmidt/httprouter"
+  "strconv"
 
   // To access the golang SDK for keystone authentication
   "github.com/rackspace/gophercloud"
   "github.com/rackspace/gophercloud/openstack"
+  "github.com/rackspace/gophercloud/pagination"
+  "github.com/rackspace/gophercloud/openstack/compute/v2/flavors"
+  "github.com/rackspace/gophercloud/openstack/compute/v2/images"
+  "github.com/rackspace/gophercloud/openstack/compute/v2/servers"
 
   // To maintain cookie for Login and Logout
   "github.com/gorilla/securecookie"
@@ -27,14 +32,25 @@ type (
   UserController struct {
     session *mgo.Session
   }
+
+  FlavorsData struct {
+    Name  string
+    ID    string
+    RAM   string
+    VCPUs string
+    Disk  string
+    RXTX  string
+  }
 )
+//Public variables
+var flavorDataList []FlavorsData
 
 // NewUserController provides a reference to a UserController with provided mongo session
 func NewUserController(s *mgo.Session) *UserController {
   return &UserController{s}
 }
 
-func keystone_admin() {
+func keystoneAdmin() {
   //Pass in the values yourself
   opts := gophercloud.AuthOptions{
       IdentityEndpoint: "http://localhost:5000/v3/",
@@ -45,13 +61,114 @@ func keystone_admin() {
       DomainName: "Default",
   }
   fmt.Print("Request details:",opts)
-
   //Once you have the opts variable, you can pass it in and get back a ProviderClient struct:
   provider, err := openstack.AuthenticatedClient(opts)
   if err != nil {
     panic(err)
   }
   fmt.Print("Authorization token details:",provider)
+}
+
+func listOpenstackDetails() {
+    //Pass in the values yourself
+    opts := gophercloud.AuthOptions{
+      IdentityEndpoint: "http://localhost:5000/v3/",
+      Username: "admin",
+      Password: "admin_user_secret",
+      TenantName: "admin",
+      DomainName: "Default",
+    }
+    //Once you have the opts variable, you can pass it in and get back a ProviderClient struct:
+    provider, err := openstack.AuthenticatedClient(opts)
+    if err != nil {
+      panic(err)
+    }
+    client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+      Region: "RegionOne",
+    })
+    if err != nil {
+      panic(err)
+    }
+
+    // We have the option of filtering the flavor list. If we want the full
+    // collection, leave it as an empty struct
+    opts1 := flavors.ListOpts{ChangesSince: "2014-01-01T01:02:03Z"}
+
+    // Retrieve a pager (i.e. a paginated collection)
+    pager := flavors.ListDetail(client, opts1)
+
+    // Define an anonymous function to be executed on each page's iteration
+    pager.EachPage(func(page pagination.Page) (bool, error) {
+      flavorList, err := flavors.ExtractFlavors(page)
+
+      for _, f := range flavorList {
+		  // "f" will be a flavors.Flavor
+		  flavorDataList = append(flavorDataList,
+			  FlavorsData{
+				  f.Name,
+				  string(f.ID),
+				  strconv.Itoa(f.RAM),
+				  strconv.Itoa(f.VCPUs),
+				  strconv.Itoa(f.Disk),
+				  strconv.FormatFloat(f.RxTxFactor, 'f', 2, 64),
+			  },
+		  )
+	  }
+      return true, err
+    })
+	fmt.Print("FlavorsList:",flavorDataList)
+    // We have the option of filtering the image list. If we want the full
+    // collection, leave it as an empty struct
+    opts2 := images.ListOpts{ChangesSince: "2014-01-01T01:02:03Z"}
+    // Retrieve a pager (i.e. a paginated collection)
+    pager1 := images.ListDetail(client, opts2)
+
+
+    // Define an anonymous function to be executed on each page's iteration
+    pager1.EachPage(func(page pagination.Page) (bool, error) {
+      imageList, err := images.ExtractImages(page)
+
+      for _, i := range imageList {
+        // "i" will be a images.Image
+        fmt.Print("\n Image Name:",i.Name)
+        fmt.Print("\n ID:",i.ID)
+        fmt.Print("\n Min.Disk:",i.MinDisk)
+        fmt.Print("\n Status:",i.Status)
+        fmt.Print("\n Progress:", i.Progress)
+        fmt.Print("\n Min.RAM:",i.MinRAM)
+        fmt.Print("\n Metadata:",i.Metadata)
+        fmt.Print("\n=============================")
+      }
+      return true, err
+    })
+
+    // We have the option of filtering the server list. If we want the full
+    // collection, leave it as an empty struct
+    opts3 := servers.ListOpts{}
+
+    // Retrieve a pager (i.e. a paginated collection)
+    pager2 := servers.List(client, opts3)
+
+    // Define an anonymous function to be executed on each page's iteration
+    pager2.EachPage(func(page pagination.Page) (bool, error) {
+      serverList, err := servers.ExtractServers(page)
+
+      for _, s := range serverList {
+        // "s" will be a servers.Server
+        fmt.Print("\n Instance Name:",s.Name)
+        fmt.Print("\n ID:",s.ID)
+        fmt.Print("\n Owner ID:",s.UserID)
+        fmt.Print("\n Image:",s.Image)
+        fmt.Print("\n Flavor:",s.Flavor)
+        fmt.Print("\n Host:", s.HostID)
+        fmt.Print("\n Status:",s.Status)
+        fmt.Print("\n Security groups:",s.SecurityGroups)
+        fmt.Print("\n Created At:",s.Created)
+        fmt.Print("\n Updated At:", s.Updated)
+        fmt.Print("\n=============================")
+      }
+      return true, err
+    })
 }
 
 
@@ -97,7 +214,7 @@ func  (uc UserController) LoginHandler(response http.ResponseWriter, request *ht
   redirectTarget := "/"
   if name != "" && pass != "" {
     if name == "Demo User" && pass == "password" {
-    keystone_admin()
+    keystoneAdmin()
     setSession(name, response)
     redirectTarget = "/dashboard"
     }
@@ -128,10 +245,11 @@ func  (uc UserController) IndexPageHandler(response http.ResponseWriter, request
 // internal page
 func Dashboard(w http.ResponseWriter, r *http.Request){
   tmpl, err := template.ParseFiles("templates/dashboard.html")
+  listOpenstackDetails()
   if err != nil {
     panic(err)
   }
-   tmpl.Execute(w, "Dashboard page")
+   tmpl.Execute(w, &flavorDataList)
 }
 
 func  (uc UserController) InternalPageHandler(response http.ResponseWriter, request *http.Request,  p httprouter.Params) {
